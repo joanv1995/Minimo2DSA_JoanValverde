0e59a06f81841d64ad5c9e409ba33770b9539e1c
joanv1995 <joanv1995@gmail.com>
2017-12-18 13:59:07 +0100

    Lista productos implementada

---
+++ app/src/main/java/com/example/usuario/minimo2dsa_joanvalverde/adapter/ProductListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.usuario.minimo2dsa_joanvalverde.adapter;
+
+/**
+ * Created by usuario on 18/12/2017.
+ */
+
+public class ProductListAdapter {
+}
---
+++ app/src/main/java/com/example/usuario/minimo2dsa_joanvalverde/model/Pedido.java
@@ -0,0 +1,8 @@
+package com.example.usuario.minimo2dsa_joanvalverde.model;
+
+/**
+ * Created by usuario on 18/12/2017.
+ */
+
+public class Pedido {
+}
